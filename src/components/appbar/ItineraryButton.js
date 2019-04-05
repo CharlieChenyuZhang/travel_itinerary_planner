18a4976531d0b1a1babb28bef8e5e27bcf496677
Denny Wiseman <d.wiseman@utoronto.ca>
2019-04-04 22:00:09 -0400

    simplify 2 buttons and fixes #131

--- src/components/appbar/ItineraryButton.js
+++ src/components/appbar/ItineraryButton.js
@@ -1,24 +1,19 @@
 import React from 'react'
-import { withStyles } from '@material-ui/core/styles'
 import IconButton from '@material-ui/core/IconButton'
 import CalendarToday from '@material-ui/icons/CalendarToday'
 
 import SearchAppBarActions from './SearchAppBarActions'
 import RecommendationsActions from '../userpage/recommendations/RecommendationsActions'
 
-const styles = theme => ({
-})
-
 class UserProfileButton extends React.Component {
   render () {
-  	 const { searchQuery, travelDate } = this.props
+    const { searchQuery, travelDate } = this.props
     return (
       <IconButton
         onClick={() => {
-        	 SearchAppBarActions.userProfileClose()
-        	 RecommendationsActions.startLoad(searchQuery, travelDate)
+          SearchAppBarActions.userProfileClose()
+          RecommendationsActions.startLoad(searchQuery, travelDate)
         }}
-        className={this.props.classes.userProfileButton}
         color='inherit'>
         <CalendarToday />
       </IconButton>
@@ -26,4 +21,4 @@ class UserProfileButton extends React.Component {
   }
 }
 
-export default withStyles(styles)(UserProfileButton)
+export default UserProfileButton
